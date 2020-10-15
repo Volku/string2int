7abd161b1a8c15b2f9725d77eae4404752caf2db
Volku <horribledark@gmail.com>
2020-10-15 13:59:18 +0700

    use Ascii Code instead

--- string2int.js
+++ string2int.js
@@ -3,23 +3,53 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-const numberic = ["0","1","2","3","4","5","6","7","8","9"]
-
+const numberic =new Map([
+    ["0",0],
+    ["1",1],
+    ["2",2],
+    ["3",3],
+    ["4",4],
+    ["5",5],
+    ["6",6],
+    ["7",7],
+    ["8",8],
+    ["9",9]
+])
+const powerOfTen=(exp) => {
+    let result = 10
+    if(exp ==0) return 1
+    while(exp>1){
+        result*=result
+        exp--
+    }
+    return result
+} 
+const toInteger=(arr)=>{
+    let result = 0
+    if(arr instanceof Array){
+        arr.forEach((num)=>{
+            result+=numberic.get(num)*powerOfTen(arr.length-(arr.indexOf(num)+1))
+        })
+        return result
+    }
+    return 0
+}
 const string2int= (word)=>{
     let characterSet = [...word]
     let answer =[]
     characterSet.forEach( function (char){
-        if(numberic.includes(char)){
-            answer.push(char);
+        let asciiCode = char.charCodeAt(0)
+        if(asciiCode>=48&&asciiCode<= 57){
+            answer.push(char)
         }
     })
-    let result =answer.reverse().join("")*1
-    console.log(result)
-    console.log("result is Integer: ", Number.isInteger(result))
+    let result =toInteger(answer)
     return result
 }
 
 rl.question("Enter your input : ",function(word){
-    string2int(word)
+    let result =string2int(word)
+    console.log(result)
+    console.log("result is Integer: ", Number.isInteger(result))
     rl.close()
 })
